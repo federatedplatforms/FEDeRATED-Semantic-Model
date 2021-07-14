11ffc8118f405ec3399a02d02fff46a2f7c6d209
bouterca <bouterca@PC-22030.tsn.tno.nl>
2021-07-14 10:27:28 +0200

    include new shacl and data files

--- shacl/pyshaclValidation.py
+++ shacl/pyshaclValidation.py
@@ -20,7 +20,7 @@ ont_graph.parse(path + 'Event.ttl', format="ttl")
 ont_graph.parse(path + 'Classifications.ttl', format="ttl")
 
 ### Validation ###
-r = validate(data_graph = data_graph, shacl_graph = shapes_graph, ont_graph = ont_graph, inference='rdfs', abort_on_error=False, meta_shacl=False, advanced=False, debug=False)
+r = validate(data_graph = data_graph, shacl_graph = shapes_graph, ont_graph = ont_graph, inference='rdfs', abort_on_error=False, meta_shacl=True, advanced=True, debug=True)
 
 conforms, results_graph, results_text = r
 
