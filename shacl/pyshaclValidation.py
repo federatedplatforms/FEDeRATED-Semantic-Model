ff79ee73aeadd43a4cfc60c96339f7330e6afdbb
Cornelis Bouter <cornelis.bouter@tno.nl>
2021-07-29 16:38:05 +0200

    update file references to newer version

--- shacl/pyshaclValidation.py
+++ shacl/pyshaclValidation.py
@@ -6,11 +6,11 @@ path = '../'
 
 ### Load data graph ###
 data_graph = rdflib.Graph()
-data_graph.parse(path + 'individuals/eventSample.ttl', format= "ttl")
+data_graph.parse(path + 'individuals/TradelensEventDataGraph.ttl', format= "ttl")
 
 ### Load shacl graph ###
 shapes_graph = rdflib.Graph()
-shapes_graph.parse('[Incident]EFTIRetrieval.shapes.ttl', format="ttl")
+shapes_graph.parse('event/event.shapes.ttl', format="ttl")
 
 ### Load all ontology graphs in one ###
 ont_graph = rdflib.Graph()
@@ -20,7 +20,7 @@ ont_graph.parse(path + 'Event.ttl', format="ttl")
 ont_graph.parse(path + 'Classifications.ttl', format="ttl")
 
 ### Validation ###
-r = validate(data_graph = data_graph, shacl_graph = shapes_graph, ont_graph = ont_graph, inference='rdfs', abort_on_error=False, meta_shacl=True, advanced=True, debug=True)
+r = validate(data_graph = data_graph, shacl_graph = shapes_graph, inference='rdfs', abort_on_error=False, meta_shacl=True, advanced=True, debug=True)
 
 conforms, results_graph, results_text = r
 
